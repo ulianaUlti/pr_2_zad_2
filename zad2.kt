32c0a729b73e907aeff81126e385b12724b2429c
ulianaUlti <124128505+ulianaUlti@users.noreply.github.com>
2023-02-13 16:52:08 +0500

    Add files via upload

---
+++ zad2.kt
@@ -0,0 +1,51 @@
+/*class Cinema (val kino:String,val seans: String,val price:Double,val zal:String,val klzrit:Int) {
+    fun Name() {
+        println(kino)
+        println(seans)
+        println(price)
+        println(zal)
+        println(klzrit)
+
+    }
+}
+fun main(){
+
+}
+*/
+
+ class Cinema {
+
+
+    val kino: String = "Аватар"
+     val seans: Double = 16.30
+   val price: Double = 100.20
+    val zal: String = "Шестой"
+   val klzrit = 30
+
+   fun person() {
+         println(klzrit * price)
+     }
+
+     fun biletMesto() {
+         println(zal)
+         println(seans)
+         println(main())
+
+     }
+
+     fun main() {
+         println(person())
+
+         println(biletMesto())
+
+
+         println(kino)
+         println(seans)
+         println(price)
+         println(zal)
+         println(klzrit)
+     }
+ }
+
+
+
